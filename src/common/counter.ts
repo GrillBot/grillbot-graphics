cc664a4e2160d539cfb4c10c19c0f53520de9d6f
Michal Halabica <m.halabica@gmail.com>
2023-02-07 22:56:01 +0100

    Simplified statistics

--- src/common/counter.ts
+++ src/common/counter.ts
@@ -1,39 +1,40 @@
 import express from 'express';
 
+interface RequestStatistics {
+    endpoint: string;
+    count: number;
+    lastRequestAt: string;
+}
+
 interface Stats {
-    totalRequestCount: number;
-    todayRequests: { count: number, date: string },
-    chartRequestsCount: number;
+    requestsCount: number;
     measuredFrom: string | null;
+    endpoints: RequestStatistics[];
 }
 
 const stats: Stats = {
-    totalRequestCount: 0,
-    todayRequests: {
-        count: 0,
-        date: ''
-    },
-    chartRequestsCount: 0,
-    measuredFrom: null
+    requestsCount: 0,
+    measuredFrom: null,
+    endpoints: []
 };
 
 export const requestsCounter = (request: express.Request, response: express.Response, next: express.NextFunction): void => {
+    stats.requestsCount++;
+
     if (!stats.measuredFrom) {
         stats.measuredFrom = new Date().toISOString();
     }
 
-    stats.totalRequestCount++;
-
-    const now = new Date();
-    const today = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
-    if (stats.todayRequests.date !== today) {
-        stats.todayRequests.date = today;
-        stats.todayRequests.count = 0;
-    }
-    stats.todayRequests.count++;
-
-    if (request.url.includes('/chart')) {
-        stats.chartRequestsCount++;
+    const endpoint = stats.endpoints.find(o => o.endpoint === request.url);
+    if (!endpoint) {
+        stats.endpoints.push({
+            count: 1,
+            lastRequestAt: new Date().toISOString(),
+            endpoint: request.url
+        });
+    } else {
+        endpoint.count++;
+        endpoint.lastRequestAt = new Date().toISOString();
     }
 
     next();
